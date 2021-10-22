afc211c153aa5242dc1fbdb9364a6df72a38be88
kuolunwang <kuolunwang.ee08@nycu.edu.tw>
2021-10-22 16:46:55 +0800

    add some packages for urr5 gazebo and update rotation code

--- ur5_bringup/src/ur5_control.py
+++ ur5_bringup/src/ur5_control.py
@@ -6,6 +6,7 @@ from arm_operation.msg import *
 from std_srvs.srv import Trigger, TriggerResponse
 from ur5_bringup.srv import *
 import tf
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import math
 
 class UR5():
@@ -28,19 +29,24 @@ class UR5():
         rad = math.pi * req.angle / 180 
         di = req.direction
 
-        for i in range(1, 11):
+        try:
+            trans, _ = self.listener.lookupTransform("base_link", "ee_link", rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+            print("Service call failed: %s"%e)
+
+        for i in range(1, 6):
 
             try:
-                trans, rot = self.listener.lookupTransform("base_link", "ee_link", rospy.Time(0))
+                _, rot = self.listener.lookupTransform("base_link", "ee_link", rospy.Time(0))
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
                 print("Service call failed: %s"%e)
 
             (row, pitch, yaw) = euler_from_quaternion(rot)
 
             if di == "right":
-                pitch += math.pi * i / 180
-            else di == "left":
-                pitch -= math.pi * i / 180
+                yaw += math.pi / 180
+            elif di == "left":
+                yaw -= math.pi / 180
 
             quat = quaternion_from_euler(row, pitch, yaw)
 
@@ -48,7 +54,7 @@ class UR5():
 
             pose = arm_operation.srv.target_poseRequest()
 
-            pose.target_pose.position.x = trans[0]
+            pose.target_pose.position.x = trans[0] + 0.18
             pose.target_pose.position.y = trans[1]
             pose.target_pose.position.z = trans[2]
             pose.target_pose.orientation.x = quat[0]
@@ -57,8 +63,10 @@ class UR5():
             pose.target_pose.orientation.w = quat[3]
 
             self.goto_pose(pose)
-            
 
+        res.result = "Finish rotation" 
+            
+        return res
 
     def get_pose(self, req):
 
@@ -80,7 +88,7 @@ class UR5():
         return res
 
     def ur5_home(self, req):
-        self.p.joint_value = [0.0011516091180965304, -2.1101391951190394, 2.3055567741394043, -3.442627255116598, -1.5785711447345179, -1.567873779927389]
+        self.p.joint_value = [0.0011875617783516645, -2.1486170927630823, 2.3022329807281494, -3.3030384222613733, -1.5724604765521448, -1.5706184546100062]
         self.mani_req.joints.append(self.p)
         res = TriggerResponse()
 
